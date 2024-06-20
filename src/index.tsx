161bd82d5cbf7c6e280d702245c32598dc7cf8fc
runawaykiwi <99219232+runawaykiwi@users.noreply.github.com>
2024-06-20 14:27:03 -0400

    Set locale

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ConfigProvider } from 'antd';
+import defaultLocale from 'antd/locale/en_US';
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ConfigProvider locale={defaultLocale}>
     <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
 
